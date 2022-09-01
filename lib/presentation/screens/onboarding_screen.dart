d9de2d1b4be0899128f5b74eec0ed758b7162223
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2022-09-01 20:30:45 +0100

    onboarding screen implementation

--- lib/presentation/screens/onboarding_screen.dart
+++ lib/presentation/screens/onboarding_screen.dart
@@ -1,3 +1,5 @@
+import 'package:crypto_app/presentation/utils/constants/colors.dart';
+import 'package:crypto_app/presentation/utils/constants/device_size.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +8,70 @@ class OnboardingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 30, left: 24),
+            child: RichText(
+                text: const TextSpan(
+                    text: 'Crypt',
+                    style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w700,
+                        color: wColor,
+                        fontSize: 64),
+                    children: [
+                  TextSpan(
+                      text: 'X',
+                      style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.w700,
+                          color: pColor,
+                          fontSize: 64))
+                ])),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 37),
+            child: Image.asset('assets/images/Shape 1.png'),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 24, bottom: 24),
+            child: Text('Jump start your\ncrypto portfolio',
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w600,
+                    color: wColor,
+                    fontSize: 32)),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(left: 24, bottom: 44),
+            child: Text('Take your investment portfolio\nto next level',
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                    color: wColor,
+                    fontSize: 14)),
+          ),
+          const Spacer(),
+          Container(
+            height: context.screenHeight() / 15,
+            alignment: Alignment.center,
+            margin: const EdgeInsets.only(left: 24, right: 24),
+            decoration: BoxDecoration(
+                color: pColor, borderRadius: BorderRadius.circular(16)),
+            child: const Text('Get Started',
+                style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.w500,
+                    color: wColor,
+                    fontSize: 16)),
+          ),
+          const Spacer(),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/presentation/utils/constants/navigators.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+//navigators
+navigatePush(BuildContext context, Widget widget) {
+  return Navigator.of(context)
+      .push(CupertinoPageRoute(builder: (context) => widget));
+}
+
+navigatePop(BuildContext context) {
+  return Navigator.pop(context);
+}
