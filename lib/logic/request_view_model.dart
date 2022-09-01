00f496acc0ebb9f71d79cc163fcb0f9b7fa06006
Cindy-dev <ikwuegbucynthia2000@gmail.com>
2022-09-01 17:45:35 +0100

    setting up controllers

--- lib/logic/request_view_model.dart
+++ lib/logic/request_view_model.dart
@@ -2,6 +2,9 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 part 'request_view_model.freezed.dart';
 
+//pkey: 9ESSQV10BX295TTNI3GO
+//skey: +yX5eV82pbSbCDlEmAIYtM5zfzNUQTat2iDQYXmS
+
 class RequestStateNotifier<T> extends StateNotifier<RequestState<T>> {
   RequestStateNotifier() : super(const RequestState.idle());
 
@@ -32,3 +35,17 @@ class RequestState<T> with _$RequestState<T> {
   const factory RequestState.error(Object error, StackTrace stackTrace) =
       Error<T>;
 }
+//all crypto id
+//https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=0023c0a2-98e2-473e-8bce-a0b2f29fb8dc
+
+//use this to get category of a coin by passing in an id
+//https://pro-api.coinmarketcap.com/v1/cryptocurrency/category?CMC_PRO_API_KEY=0023c0a2-98e2-473e-8bce-a0b2f29fb8dc
+
+//list of all categories
+//https://pro-api.coinmarketcap.com/v1/cryptocurrency/categories?CMC_PRO_API_KEY=0023c0a2-98e2-473e-8bce-a0b2f29fb8dc
+
+//
+
+
+
+//0023c0a2-98e2-473e-8bce-a0b2f29fb8dc
